570e4044b1e9b3892abd15eeef0b8c52015763bb
pelev1n <pelevin9911@gmail.com>
2018-08-28 22:03:28 +0300

    Added record logic

--- app/src/main/java/com/example/user/grafacc/GrafActivity.java
+++ app/src/main/java/com/example/user/grafacc/GrafActivity.java
@@ -13,11 +13,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class GrafActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager mSensorManager;
@@ -38,12 +44,17 @@ public class GrafActivity extends AppCompatActivity implements SensorEventListen
     Button showRecord;
     TextView recordResult;
 
+    private boolean state;
+
+    private final static String FILE_NAME = "filename.txt";
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graf);
+        state = false;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         startRecord = (Button) findViewById(R.id.start_record);
         stopRecord = (Button) findViewById(R.id.stop_record);
@@ -178,4 +189,58 @@ public class GrafActivity extends AppCompatActivity implements SensorEventListen
         thread.interrupt();
         super.onDestroy();
     }
+
+    public void saveText(View view) {
+
+        FileOutputStream fos = null;
+        try {
+            EditText textBox = (EditText) findViewById(R.id.save_text);
+            String text = textBox.getText().toString();
+
+            fos = openFileOutput(FILE_NAME, MODE_APPEND);
+            fos.write(text.getBytes());
+            Toast.makeText(this, "Файл сохранен", Toast.LENGTH_SHORT).show();
+        } catch (IOException ex) {
+
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        } finally {
+            try {
+                if (fos != null)
+                    fos.close();
+            } catch (IOException ex) {
+
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // открытие файла
+    public void openText(View view) {
+
+        FileInputStream fin = null;
+        TextView textView = (TextView) findViewById(R.id.open_text);
+        try {
+            fin = openFileInput(FILE_NAME);
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String(bytes);
+            textView.setText(text);
+        } catch (IOException ex) {
+
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        } finally {
+
+            try {
+                if (fin != null)
+                    fin.close();
+            } catch (IOException ex) {
+
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+
+
 }
